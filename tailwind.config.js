0552b80e96c8c522631d596b56ca55a6e39ff874
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-10-10 22:57:22 +0900

    トップページ途中２分の１完了

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
     extend: {
       colors: {
         primary: '#5A7B8C',  // 例: プライマリーカラーの定義
-        secondary: '#EDEDED', // 例: セカンダリーカラーの定義      
+        secondary: '#EDEDED', // 例: セカンダリーカラーの定義  
+        button: '#4C6B5C',
+        subbutton: '#A3C1DA'    
       },
     },
   },
