761a670dcac21f3346f9e5959288591fdac658d9
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-10-13 05:39:22 +0900

    ログイン前のヘッダー作成完了

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
         primary: '#5A7B8C',  // 例: プライマリーカラーの定義
         secondary: '#EDEDED', // 例: セカンダリーカラーの定義  
         button: '#4C6B5C',
-        subbutton: '#A3C1DA'    
+        subbutton: '#A3C1DA',
+        menu:'#4A6B7C'
+
       },
       fontFamily: {
         Spicyrice: ['Spicy Rice', 'serif'],
