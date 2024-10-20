66215b3fafbb2cbc4e02bd71b70d852e08d04e64
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-10-20 23:55:16 +0900

    ユーザー登録時のエラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,13 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to root_path
     else
-      render :new
+      redirect_to action: :new
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username,email,password,password_confirmation)
+    params.require(:user).permit(:username,:email,:password,:password_confirmation)
   end
 end
