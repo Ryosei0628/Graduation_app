e658c47688b3d87f3970cbae9c6eeed8f755fdc0
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-10-28 04:00:57 +0900

    バリデーションとマイグレーションの設定

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ApplicationRecord
+  validates :location_name, presence: true, length: { maximum: 255 }
+  validates :address, presence: true
+  validates :start_hour, :end_hour, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 24 }
+  validates :start_minute, :end_minute, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 60 }
+  validates :description, presence: true, length: { maximum: 65_535 }
+  validates :quiet_level, presence: true
+
+
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_17_071622) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_27_173935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "location_name", null: false
+    t.string "address", null: false
+    t.float "latitude", null: false
+    t.float "longitude", null: false
+    t.string "start_hour"
+    t.string "start_minute"
+    t.string "end_hour"
+    t.string "end_minute"
+    t.text "description", null: false
+    t.string "wifi"
+    t.string "electricity"
+    t.string "site_url"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -23,4 +42,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_17_071622) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
