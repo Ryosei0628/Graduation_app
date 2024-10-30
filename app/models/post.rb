973a64a3502ecc607cce2244d207ea3cbe261b69
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-10-31 07:19:16 +0900

    add：作成画面の作成完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,10 @@ class Post < ApplicationRecord
   validates :quiet_level, presence: true
   validates :genre, presence: true
 
+  enum genre: { cafe: 0, library: 1, park: 2, coworking_space: 3, study_room: 4, other: 5 } 
+  enum wifi: {available: 0, not_available: 2}
+  enum electricity: {is_available: 0, isnot_available: 2}
+
 
   belongs_to :user
 end
