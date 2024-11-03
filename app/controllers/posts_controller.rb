39d68476c69e65858a0b51f37584e1467f76ba91
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-11-03 13:49:29 +0900

    add: 掲示板一覧機能の作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,19 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: t('defaults.flash_message.created', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:location_name, :address, :start_hour, :start_minute, :end_hour, :end_minute, :description, :wifi, :electricity, :site_url, :genre, :quiet_level)
+  end
 end
