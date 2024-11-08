8c5f59b19c6047c55f6f6556b89c96c1efb1ad02
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-11-08 11:55:47 +0900

    画像投稿機能実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
   validates :quiet_level, presence: true
   validates :genre, presence: true
 
-  enum genre: { cafe: 0, library: 1, park: 2, coworking_space: 3, study_room: 4, other: 5 } 
+  enum genre: { cafe_place: 0, library: 1, park: 2, coworking_space: 3, study_room: 4, other: 5 } 
   enum wifi: {available: 0, not_available: 2}
   enum electricity: {is_available: 0, isnot_available: 2}
   enum quiet_level: {noisy: 1,# 日常の騒がしさ　
@@ -18,4 +18,5 @@ class Post < ApplicationRecord
 
 
   belongs_to :user
+  mount_uploader :post_image, PostImageUploader
 end
