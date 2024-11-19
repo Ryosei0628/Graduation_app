84a9e8b6fd949b441b32fa2f325ec8d805725a03
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-11-20 06:03:55 +0900

    style: 閉じるボタンの削除

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,14 +5,11 @@ import "./controllers"
 document.addEventListener('turbo:load', () => {
     const button = document.getElementById('button');
     const bars = document.getElementById('bars');
-    const x = document.getElementById('x');
     const menu = document.getElementById('menu');
     const overlay = document.getElementById('overlay');
 
     if (button) {
         button.addEventListener('click', event => {
-            bars.classList.toggle('hidden');
-            x.classList.toggle('hidden');
             menu.classList.toggle('right-[-20rem]');
             menu.classList.toggle('right-0');
             overlay.classList.toggle('hidden');
@@ -24,7 +21,6 @@ document.addEventListener('turbo:load', () => {
         menu.classList.remove('right-0'); 
         overlay.classList.add('hidden');  // オーバーレイを非表示
         bars.classList.remove('hidden');
-        x.classList.add('hidden');
     });
 });
 
