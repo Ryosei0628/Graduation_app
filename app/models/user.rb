e915be05e932d9a8e4ab1d0e39055e6f3d517cfc
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-12-06 01:16:55 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :posts, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmark_posts, through: :bookmarks, source: :post
   
   def own?(post)
     posts.include?(post)  # 投稿がユーザーのものかどうかをチェック
