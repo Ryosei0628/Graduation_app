d66c2978fa3e07165cf3b1381b5e8eb2cf8d9cd8
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-12-19 00:22:46 +0900

    add: 動的ogpの作成完了

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,36 +3,48 @@ module ApplicationHelper
     assign_meta_tags if display_meta_tags.blank?
     display_meta_tags
   end
-
-  def default_meta_tags
-    def default_meta_tags
-    {
-      site: 'zenfocus',
-      title: '集中できる静かな場所を探す',
-      reverse: true,
-      separator: '|',   #Webサイト名とページタイトルを区切るために使用されるテキスト
-      description: '勉強や作業に最適な静かな場所を簡単に見つけられるサービスです。',
-      keywords: '静かな場所, 勉強, 作業, カフェ, 図書館, zenfocus',   #キーワードを「,」区切りで設定する
-      canonical: request.original_url,   #優先するurlを指定する
-      noindex: ! Rails.env.production?,
-      icon: [                    #favicon、apple用アイコンを指定する
-        { href: image_url('favicon.ico') },
-        { href: image_url('icon.jpg'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/jpg' },
-      ],
-      og: {
-        site_name: 'zenfocus',
-        title: '集中できる静かな場所を探す | zenfocus',
-        description: '勉強や作業にぴったりな静かな場所を地図検索で簡単に探せるサービスです。', 
-        type: 'website',
-        url: request.original_url,
-        image: image_url('ogp.png'),
-        locale: 'ja_JP',
-      },
-      twitter: {
-        card: 'summary_large_image',
-      }
+    
+  def assign_meta_tags(options = {})
+    defaults = {
+      site: 'サイト名',
+      title: 'デフォルトのタイトル',
+      description: 'デフォルトのページの説明',
+      keywords: 'デフォルトのページキーワード',
+      image: image_url('logo.png') # デフォルト画像
     }
-  end
+    options.reverse_merge!(defaults)
+    site = options[:site]
+    title = options[:title]
+    description = options[:description]
+    keywords = options[:keywords]
+    image = options[:image].presence || image_url('logo.png')
+    configs = {
+      separator: '|',
+      reverse: true,
+      site:,
+      title:,
+      description:,
+      keywords:,
+      canonical: request.original_url,
+      icon: {
+        href: image_url('logo.png')
+        },
+      og: {
+        type: 'website',
+        title: title.presence || site,
+        url: request.original_url,
+        image:,
+        site_name: site
+        description: nil  # OGPのdescriptionは空にする
+        },
+      twitter: {
+        site:,
+        card: 'summary_large_image',
+        image:
+        description: nil  # Twitterのdescriptionも空にする
+         }
+    }
+      set_meta_tags(configs)
   end
 end
 
