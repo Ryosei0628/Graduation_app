c206af3fc3d705f54a464d45eb47e95071ac34df
Ryosei Muramatsu <ryo06280628sesei@gmail.com>
2024-12-22 18:17:50 +0900

    既存のデータを新しいカラムに統合する処理を追加

--- db/migrate/20241220144029_add_start_and_end_time_to_posts.rb
+++ db/migrate/20241220144029_add_start_and_end_time_to_posts.rb
@@ -1,6 +1,22 @@
 class AddStartAndEndTimeToPosts < ActiveRecord::Migration[7.2]
-  def change
+  def up
     add_column :posts, :start_time, :datetime
     add_column :posts, :end_time, :datetime
+
+    Post.reset_column_information
+
+    Post.find_each do |post|
+      if post.start_hour && post.start_minute && post.end_hour && post.end_minute
+        post.update(
+          start_time: Time.zone.parse("#{post.start_hour}:#{post.start_minute}"),
+          end_time: Time.zone.parse("#{post.end_hour}:#{post.end_minute}")
+        )
+      end
+    end
+  end
+
+  def down
+    remove_column :posts, :start_time
+    remove_column :posts, :end_time
   end
 end
